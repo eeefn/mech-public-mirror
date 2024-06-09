7c89f7b4a82fefb25ff0407ddefd5380cd8e163b
unknown <ethangreeff@gmail.com>
2024-06-09 12:18:08 -0600

    move window create and destroy to WindowManager

---
+++ mech/src/WindowManager.cpp
@@ -0,0 +1,35 @@
+#include "../headers/WindowManager.h"
+#include <stdio.h>
+#include "../headers/constants.h"
+WindowManager windowManager;
+
+bool WindowManager::initializeWindow() {
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		fprintf(stderr, "Error initializing SDL\n");
+		return false;
+	}
+	window = SDL_CreateWindow(
+		NULL,
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		WINDOW_WIDTH,
+		WINDOW_HEIGHT,
+		SDL_WINDOW_BORDERLESS
+	);
+	if (!window) {
+		fprintf(stderr, "Error initializing window\n");
+		return false;
+	}
+	renderer = SDL_CreateRenderer(window, -1, 0);
+	if (!renderer) {
+		fprintf(stderr, "Error initializing renderer\n");
+		return false;
+	}
+	return true;
+}
+
+void WindowManager::destroyWindow() {
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
