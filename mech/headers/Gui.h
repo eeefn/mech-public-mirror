63aa59d99d326c99483af673bd334fc0ea5b2b74
unknown <ethangreeff@gmail.com>
2024-06-14 18:36:12 -0600

    rm unused functions in gui that were moved to the editor class

--- mech/headers/Gui.h
+++ mech/headers/Gui.h
@@ -1,24 +1,12 @@
 #pragma once
 #include <SDL.h>
-#include "../headers/entities/Entity.h"
 
 class Gui
 {
 	public:
 		//array of rects for displaying the right tile from gui
 		void render(int gameMode);
-		void renderEditorSelection();
 		void renderSoul();
-		void setupSelector(Entity* playerEntity);
-		void expandSelWindowLeft();
-		void expandSelWindowRight();
-		void expandSelWindowDown();
-		void expandSelWindowUp();
-		void moveSelWindowLeft();
-		void moveSelWindowRight();
-		void moveSelWindowDown();
-		void moveSelWindowUp();
-		SDL_Rect* getSelWindowRen();
 		int soulColor;
 		int pulseCount;
 		short selectColor;
