4487c70e9c84e7458d37c8eab6222744612d4b04
eeefn <137131699+eeefn@users.noreply.github.com>
2024-08-07 17:57:09 -0800

    added file to define GameObjectNames

--- mech/headers/gameObjects/GameObjectManager.h
+++ mech/headers/gameObjects/GameObjectManager.h
@@ -6,18 +6,20 @@
 #include "Mushroom.h"
 #include "Portal.h"
 #include "Rock.h"
+#include "GameObjectCodes.h"
 
+using namespace gameObjectCodes;
 class GameObjectManager{
     public:
         GameObjectManager();
         ~GameObjectManager();
         using ObjectFactory = GameObject*(*)(int id, short xT, short yT);
         std::unordered_map<int,ObjectFactory> objectFactory = {
-            {1, [](int id, short xT, short yT) -> GameObject* {Mushroom* mush = new Mushroom(0,id,xT,yT); return mush;}}, 
-            {2, [](int id, short xT, short yT) -> GameObject* {Portal* port = new Portal(id,xT,yT);return port;}},
-            {3, [](int id, short xT, short yT) -> GameObject* {Rock* rock0 = new Rock(id,xT,yT,0);return rock0;}},
-            {4, [](int id, short xT, short yT) -> GameObject* {Rock* rock1 = new Rock(id,xT,yT,1);return rock1;}},
-            {5, [](int id, short xT, short yT) -> GameObject* {Rock* rock2 = new Rock(id,xT,yT,2);return rock2;}}
+            {MUSHROOM, [](int id, short xT, short yT) -> GameObject* {Mushroom* mush = new Mushroom(0,id,xT,yT); return mush;}}, 
+            {PORTAL, [](int id, short xT, short yT) -> GameObject* {Portal* port = new Portal(id,xT,yT);return port;}},
+            {ROCK_0, [](int id, short xT, short yT) -> GameObject* {Rock* rock0 = new Rock(id,xT,yT,0);return rock0;}},
+            {ROCK_1, [](int id, short xT, short yT) -> GameObject* {Rock* rock1 = new Rock(id,xT,yT,1);return rock1;}},
+            {ROCK_2, [](int id, short xT, short yT) -> GameObject* {Rock* rock2 = new Rock(id,xT,yT,2);return rock2;}}
         };
         void manageHighlightedObjects(SDL_Rect* hitBox,const string& entityId);
         void removeObject(GameObject* objToRemove);
