ccd5fce3dd56e9e834bf46e5abb9a7b4d9d133af
eeefn <137131699+eeefn@users.noreply.github.com>
2024-08-09 11:10:37 -0800

    added stick item

--- mech/headers/items/ItemManager.h
+++ mech/headers/items/ItemManager.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Item.h"
 #include "RockItem.h"
+#include "StickItem.h"
 #include "ItemCodes.h"
 #include <unordered_map>
 
@@ -11,7 +12,8 @@ class ItemManager{
         Item* makeItem(int itemCode, int itemCount);
         using ItemFactory = Item*(*)(int itemCount);
         std::unordered_map<int,ItemFactory> itemFactory = {
-            {ROCK, [](int itemCount) -> Item* {RockItem* rock = new RockItem(itemCount); return rock;}} 
+            {ROCK, [](int itemCount) -> Item* {RockItem* rock = new RockItem(itemCount); return rock;}},
+            {STICK,[](int itemCount) -> Item* {StickItem* stick = new StickItem(itemCount);return stick;}}
         };
 };
 
