e9517f8a513ed44527a7ed9b2ea6f82f27de49d4
eeefn <137131699+eeefn@users.noreply.github.com>
2024-09-15 12:15:41 -0800

    recipes can now be scrolled through within the crafting window

--- mech/src/CraftingWindow.cpp
+++ mech/src/CraftingWindow.cpp
@@ -2,20 +2,25 @@
 #include "../headers/TextureManager.h"
 #include "../headers/WindowManager.h"
 #include "../headers/items/ItemFactory.h"
+#include <stdlib.h>
 
 CraftingWindow::CraftingWindow(int invPosOnScreenX, int invPosOnScreenY){
     basePanel.posOnTexture = {0,75,108,75};
     basePanel.posOnScreen = {invPosOnScreenX - (scale * basePanel.posOnTexture.w), invPosOnScreenY, scale*basePanel.posOnTexture.w, scale*basePanel.posOnTexture.h};
     scrollBar.posOnTexture = {198,18,3,7}; 
     scrollBar.posOnScreen = {basePanel.posOnScreen.x + 101*scale, basePanel.posOnScreen.y + 8*scale, scale*scrollBar.posOnTexture.w, scale*scrollBar.posOnTexture.h};
-    minScrollOffset += scrollBar.posOnScreen.y;
-    maxScrollOffset += scrollBar.posOnScreen.y;
+    minScrollBarOffset += scrollBar.posOnScreen.y;
+    maxScrollBarOffset += scrollBar.posOnScreen.y;
     recipePanel.posOnTexture = {109,75,88,20};
     recipePanel.posOnScreen = {basePanel.posOnScreen.x + 9*scale, basePanel.posOnScreen.y + 9*scale, scale*recipePanel.posOnTexture.w,scale*recipePanel.posOnTexture.h};
-    minYWithinRecipePane += basePanel.posOnScreen.y + scale * 6;
-    maxYWithinRecipePane += basePanel.posOnScreen.y + scale * 6;
+    minYWithinRecipePane += basePanel.posOnScreen.y + scale * 8;
+    maxYWithinRecipePane += basePanel.posOnScreen.y + scale * 8;
     Recipe testRecipe = {{ItemCodes::STICK,1},{ItemCodes::ROCK,1},{ItemCodes::GARDENBOX,1}};
+    Recipe testRecipe1 = {{ItemCodes::YUCCALEAF,1},{ItemCodes::YUCCALEAF,1},{ItemCodes::GARDENBOX,1}};
+    Recipe testRecipe2 = {{ItemCodes::STICK,1},{ItemCodes::ROCK,1},{ItemCodes::GARDENBOX,1}};
     currentlyCraftableRecipes.push_back(testRecipe);
+    currentlyCraftableRecipes.push_back(testRecipe1);
+    currentlyCraftableRecipes.push_back(testRecipe2);
 }
 
 void CraftingWindow::render(){
@@ -24,43 +29,80 @@ void CraftingWindow::render(){
     renderVisibleRecipes();
 }
 
-void CraftingWindow::scrollUp(){
-    scrollBar.posOnScreen.y += scale*3;        
-    if(scrollBar.posOnScreen.y > maxScrollOffset){
-        scrollBar.posOnScreen.y = maxScrollOffset;
+void CraftingWindow::scrollDown(){
+    scrollBar.posOnScreen.y += scale*3;
+    if(scrollBar.posOnScreen.y > maxScrollBarOffset){
+        scrollBar.posOnScreen.y = maxScrollBarOffset;
     }
+    paneOffset -= scale*3; 
 }
 
-void CraftingWindow::scrollDown(){
+void CraftingWindow::scrollUp(){
     scrollBar.posOnScreen.y -= scale*3;
-    if(scrollBar.posOnScreen.y < minScrollOffset){
-        scrollBar.posOnScreen.y = minScrollOffset;
+    if(scrollBar.posOnScreen.y < minScrollBarOffset){
+        scrollBar.posOnScreen.y = minScrollBarOffset;
     }
+    paneOffset += scale*3;
 }
 
 void CraftingWindow::update(){
 }
 
 void CraftingWindow::renderVisibleRecipes(){
-    for(auto recipe : currentlyCraftableRecipes){
-        SDL_RenderCopy(windowManager.renderer,textureManager.inventoryTexture,&recipePanel.posOnTexture,&recipePanel.posOnScreen);
-        renderRecipeOnPanel(recipe);
+    for(unsigned int i = 0; i < currentlyCraftableRecipes.size(); i++){
+        Recipe currentRecipe = currentlyCraftableRecipes.at(i);
+        recipePanel.posOnScreen.y += paneOffset + (recipePanel.posOnScreen.h * i);
+        renderElementWithinPane(*textureManager.inventoryTexture,recipePanel);
+        renderRecipeOnPanel(currentRecipe);
+        recipePanel.posOnScreen.y -= paneOffset + (recipePanel.posOnScreen.h * i);
     }
 }
 
 void CraftingWindow::renderRecipeOnPanel(Recipe& recipe){
-    Item* mockItem = itemFactory.makeItem(recipe.firstIngredient.itemCode,recipe.firstIngredient.quantity);
-    itemPosOnScreen.x = recipePanel.posOnScreen.x + (2 * scale); 
+    renderIngredientOnPanel(recipe.firstIngredient, 2 * scale);
+    renderIngredientOnPanel(recipe.secondIngredient,35 * scale);
+    renderIngredientOnPanel(recipe.result, 69 * scale);
+}
+
+int CraftingWindow::calcChopVal(SDL_Rect& posOnScreen){
+    int chopVal = 0;
+    if(posOnScreen.y <= minYWithinRecipePane){
+        chopVal = (posOnScreen.y - minYWithinRecipePane) / 3;
+    }
+    else if(posOnScreen.y + posOnScreen.h >= maxYWithinRecipePane){
+        chopVal = (posOnScreen.y + posOnScreen.h - maxYWithinRecipePane) / 3;
+    }
+    return chopVal;
+}
+
+void CraftingWindow::renderIngredientOnPanel(Ingredient& ingredient, int xOffset){
+    Item* mockItem = itemFactory.makeItem(ingredient.itemCode,ingredient.quantity);
+    itemPosOnScreen.x = recipePanel.posOnScreen.x + xOffset; 
     itemPosOnScreen.y = recipePanel.posOnScreen.y + (2 * scale); 
-    SDL_RenderCopy(windowManager.renderer,textureManager.itemsTexture,mockItem->getSpriteSheetPos(),&itemPosOnScreen);
+    SDL_Rect posOnTexture = *mockItem->getSpriteSheetPos();
+    RenderRect renRect = {posOnTexture,itemPosOnScreen};
+    renderElementWithinPane(*textureManager.itemsTexture,renRect);
     delete mockItem;
-    mockItem = itemFactory.makeItem(recipe.secondIngredient.itemCode,recipe.secondIngredient.quantity);
-    itemPosOnScreen.x = recipePanel.posOnScreen.x + ((2 + 32 + 1) * scale); 
-    itemPosOnScreen.y = recipePanel.posOnScreen.y + (2 * scale); 
-    SDL_RenderCopy(windowManager.renderer,textureManager.itemsTexture,mockItem->getSpriteSheetPos(),&itemPosOnScreen);
-    delete mockItem;
-    mockItem = itemFactory.makeItem(recipe.result.itemCode,recipe.result.quantity);
-    itemPosOnScreen.x = recipePanel.posOnScreen.x + ((2 + 64 + 3) * scale); 
-    itemPosOnScreen.y = recipePanel.posOnScreen.y + (2 * scale); 
-    SDL_RenderCopy(windowManager.renderer,textureManager.itemsTexture,mockItem->getSpriteSheetPos(),&itemPosOnScreen);
+}
+
+void CraftingWindow::renderElementWithinPane(SDL_Texture& tex,RenderRect rect){
+    int chopVal = calcChopVal(rect.posOnScreen);
+    if(abs(chopVal) <= rect.posOnTexture.h){
+        if(chopVal == 0){
+            SDL_RenderCopy(windowManager.renderer,&tex,&rect.posOnTexture,&rect.posOnScreen);
+        }
+        else if(chopVal < 0){
+            chopVal = chopVal * -1;
+            rect.posOnTexture.y += chopVal;
+            rect.posOnTexture.h -= chopVal;
+            rect.posOnScreen.y += chopVal * scale;
+            rect.posOnScreen.h -= chopVal * scale;
+            SDL_RenderCopy(windowManager.renderer,&tex,&rect.posOnTexture,&rect.posOnScreen);
+        }
+        else{
+            rect.posOnTexture.h -= chopVal;
+            rect.posOnScreen.h -= chopVal * scale;
+            SDL_RenderCopy(windowManager.renderer,&tex,&rect.posOnTexture,&rect.posOnScreen);
+        }
+    }
 }
