cdcac1e40d81e6bd7495079fc30f0ce4739c8f4d
eeefn <137131699+eeefn@users.noreply.github.com>
2024-09-23 13:25:13 -0800

    Portal requires 5 soulShards to activate

--- mech/headers/gameObjects/Portal.h
+++ mech/headers/gameObjects/Portal.h
@@ -9,10 +9,13 @@ class Portal : public GameObject{
         bool activate();
         void render(SDL_Renderer *rend);
 		bool highlight(const std::string& srcEntityId);
+        void handleClick(Item *clickedBy);
         void update();
         bool active = false;
         Animator anim;
         Animator::AnimSelect discs{0,0};
         Animator::AnimSelect beam{1,0};
         SDL_Rect portalAnimRect[2][55];
+    private:
+        int shardsHeld = 0; 
 };
